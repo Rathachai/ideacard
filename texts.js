19869cbf9e322841ea26a0c7a4eed5a0a34fed09
Rathachai CHAWUTHAI <rathachai.c@gmail.com>
2020-06-08 09:31:03 +0700

    init

---
+++ texts.js
@@ -0,0 +1,16 @@
+texts = [
+	["Food","อาหาร","What"], 
+	["Drink","เครื่องดื่ม","What"],
+	["Cloth","เสื้อผ้า","What"],
+	["Buildings","สิ่งก่อสร้าง","What"],
+	["Reading","สินค้าสำหรับอ่าน","What"],
+	["Musics & Songs","ดนตรี และ เพลง","What"],
+	["Smell","เกี่ยวกับกลิ่น","What"],
+	["Pressing","สิ่งที่กดได้","What"],
+	["Show","เอาไว้โชว์","What"],
+	["Runing","วิ่ง","What"],
+	["Laugh","หัวเราะ","What"],
+	["Cry","ร้องไห้","What"],
+	["Appreciate","ชื่นชม","What"],
+	["Emulate","เลียนแบบ","What"]
+];
